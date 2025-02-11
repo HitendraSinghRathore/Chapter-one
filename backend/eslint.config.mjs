0ecb28f01f4e82abdb13209e0d2bc5484338605e
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-02-11 22:37:50 +0530

    update backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      quotes: ['error', 'single', { avoidEscape: true }],
+    }
+  }
+);
