64f51bbc3bd941d1f7cd98706231e6582816f360
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-02-16 23:22:18 +0530

    add frontend serve

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import path from 'path';
 
 
 const app = express();
@@ -9,6 +10,13 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
 
 
 app.get('/healthz', (req: Request, res: Response) => {
