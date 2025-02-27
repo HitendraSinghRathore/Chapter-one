9ba8f47230c9b7e70fc38eeed910e5dbf5e6e81e
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-02-27 18:55:32 +0530

    update changes for signup

--- frontend/chapter-one/src/app/book.service.ts
+++ frontend/chapter-one/src/app/book.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,7 +11,7 @@ export class BookService {
   private apiUrl = environment.apiUrl + '/books';
 
   constructor(private http: HttpClient) {}
-
+  
   getBooks(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
